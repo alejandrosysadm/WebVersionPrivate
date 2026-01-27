9cb0e57117dcdbd06c093623d4419ed2688dd8cb
alejandrosysadm <alejandro.ferrandiz@icloud.com>
2026-01-27 01:17:43 +0100

    subida ficheros movil

--- js/script.js
+++ js/script.js
@@ -85,3 +85,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     if (target) { e.preventDefault(); target.scrollIntoView({ behavior: 'smooth' }); }
   });
 });
+
+// MENU HAMBURGUESA
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  navMenu.classList.toggle('active');
+});
+
+// Cerrar menú al hacer click
+document.querySelectorAll('#nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('open');
+    navMenu.classList.remove('active');
+  });
+});
